95a13cb11d6fc60288c1f08e5ecefc394db3f391
JuanMa <jmg216@hotmail.com>
2014-05-24 19:40:41 -0300

    Este es un comentario.

--- src/com/home/archivos/Main.java
+++ src/com/home/archivos/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		ArchivosTextosPlanos atp = new ArchivosTextosPlanos();
 		
+		//Estoy haciendo una prueba
 		atp.crearArchivo();
 		atp.escribirArchivo();
 		atp.leerArchivo();
